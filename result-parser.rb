13654f256fe336e76d57ce4934f3bfc0e1da0d53
S.Tokunaga <otokuna46@gmail.com>
2015-05-21 13:32:18 +0900

    fixing result parser

--- result-parser.rb
+++ result-parser.rb
@@ -1,22 +1,23 @@
 class ResultParser
   attr_reader :matched_list, :stored_hash
+  
   def initialize()
     @matched_list = []
   end
   def read_file(file_name)
     File.open(file_name) do |file|
       file.each do |line|
-        if line.match(/(?<elderly state>):(?<number>\d?)/) 
-          current_state = line.match(/(?<elderly state>):(?<number>\d?)/)[2].to_i
-          watched_state = line.match(/(?<judged\_state>):(?<watched_number>\d)/)[2].to_i
-          if current_state == watched_state
-            p "tp"
-          else 
-            p "tn"
+        unless line == nil then
+          if line.match(/(?<elderly\_state>)/) != nil
+            p current_state = line.match(/(?<elderly\_state>):(?<number>\d?)/)
+            p current_state["number"]
+            watched_state = line.match(/(?<watched\_state>):(?<number>\d?)/)
+            p watched_state
+            p watched_state["number"]
           end
+        end
          # p line.match(/(?<elderly state>):(?<number>\d?)/)[2] 
          # p line.match(/(?<judged\_state>):(?<watched_number>\d)/)[2]
-        end
       end
     end
   end
