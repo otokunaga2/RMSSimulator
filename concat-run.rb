2c4b34fe22bbe2d6c49f1c03071451f5f562ad38
S.Tokunaga <otokuna46@gmail.com>
2015-05-29 17:52:36 +0900

    create simulate func

--- concat-run.rb
+++ concat-run.rb
@@ -1,12 +1,26 @@
 require './run.rb'
 
-main_instance = Main.new
-main_instance.simulation_number.to_i.times do
-  main_instance.simulate
+
+def exec_simulate(simulation_obj)
+  simulation_obj.simulation_number.to_i.times do
+    simulation_obj.simulate
+  end
 end
 
-main_instance_for_setting2 = Main.new(setting_file_name: "setting2.txt")
-main_instance_for_setting2.simulation_number.to_i.times do
-  main_instance_for_setting2.simulate
-end
+main_instance_for_china = Main.new(setting_file_name: "setting_china.txt")
+main_instance_for_africa= Main.new(setting_file_name: "setting_africa.txt")
+main_instance_for_ghana = Main.new(setting_file_name: "setting_ghana.txt")
+main_instance_for_india= Main.new(setting_file_name: "setting_india.txt")
+main_instance_for_russia= Main.new(setting_file_name: "setting_russia.txt")
+main_instance_for_mexico= Main.new
+
+exec_simulate(main_instance_for_mexico)
+exec_simulate(main_instance_for_china)
+exec_simulate(main_instance_for_africa)
+exec_simulate(main_instance_for_ghana)
+exec_simulate(main_instance_for_india)
+exec_simulate(main_instance_for_russia)
+
+
+
 
