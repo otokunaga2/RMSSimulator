9831bc29971b137ea192d5c262d1add814ef163f
Seiki Tokunaga <otokuna46@gmail.com>
2015-06-05 08:09:14 +0900

    リファクタリング（インスタンス変数をローカル変数に変更）

--- concat-run.rb
+++ concat-run.rb
@@ -1,17 +1,21 @@
 require './run.rb'
 
+#シミュレーションを実行するインスタンスを格納するためのリスト
 main_instance_list = []
-for i in 0..1000 do
+#シミュレーションを実行するインスタンスの生成
+for i in 0..100 do
   main_instance_list.push(Main.new(init_setting_file_name: 'setting.txt'))
 end
 
 def exec_simulation(main_instance)
-  main_instance.restore_outputfile(prefix: "setting.txt")
+  #main_instance.restore_outputfile(prefix: "setting.txt")
+  #設定ファイルに設定されたシミュ-レーション回数分、シミュレーションを実行
   main_instance.simulation_number.to_i.times do
     main_instance.simulate
   end
 end
 
+#格納されたリストの数だけ、シミュレーション結果をファイルとして生成
 main_instance_list.each do |main_instance|
   exec_simulation(main_instance)  
 end
--- run.rb
+++ run.rb
@@ -13,41 +13,40 @@ class Main
     init_property(setting_file_name: init_setting_file_name)
   end
 
+  #設定ファイルから変数に格納するための初期関数
   def init_property(setting_file_name: 'setting.txt')
     target_word_list = %w[alpha beta gamma simulation_number 
                           gradient second_gradient third_gradient firststate
                           fail_ill_ratio fail_healthy_ratio]
-    @file_read_instance = SettingReader.new(setting_file_name,target_word_list)
-    @file_read_instance.store_to_hash
-    @alpha = @file_read_instance.stored_hash["alpha"]
-    @beta = @file_read_instance.stored_hash["beta"]
-    @gamma = @file_read_instance.stored_hash["gamma"]
-    @simulation_number = @file_read_instance.stored_hash["simulation_number"]
-    @gradient = @file_read_instance.stored_hash["gradient"]
-    @second_gradient = @file_read_instance.stored_hash["second_gradient"]
-    @third_gradient = @file_read_instance.stored_hash["third_gradient"]
-    firststate = @file_read_instance.stored_hash["firststate"]
-    @elderly = Elderly.new(alpha: @alpha,beta: @beta,gamma: @gamma,
+    file_read_instance = SettingReader.new(setting_file_name,target_word_list)
+    file_read_instance.store_to_hash
+    alpha = file_read_instance.stored_hash["alpha"]
+    beta = file_read_instance.stored_hash["beta"]
+    gamma = file_read_instance.stored_hash["gamma"]
+    @simulation_number = file_read_instance.stored_hash["simulation_number"]
+    gradient = file_read_instance.stored_hash["gradient"]
+    second_gradient = file_read_instance.stored_hash["second_gradient"]
+    third_gradient = file_read_instance.stored_hash["third_gradient"]
+    firststate = file_read_instance.stored_hash["firststate"]
+    @elderly = Elderly.new(alpha: alpha,beta: beta,gamma: gamma,
                            simulation_number: @simulation_number,
-                           gradient: @gradient,second_gradient: @second_gradient,
-                           third_gradient: @third_gradient,first_state: firststate)
+                           gradient: gradient,second_gradient: second_gradient,
+                           third_gradient: third_gradient,first_state: firststate)
     #@elderly.current_state = 0
-    fail_healthy_ratio= @file_read_instance.stored_hash["fail_healthy_ratio"]
-    fail_ill_ratio = @file_read_instance.stored_hash["fail_ill_ratio"]
-    @random_instance = Random.new(1)
+    fail_healthy_ratio= file_read_instance.stored_hash["fail_healthy_ratio"]
+    fail_ill_ratio = file_read_instance.stored_hash["fail_ill_ratio"]
+    #@random_instance = Random.new(1)
     set_hash(fail_healthy_ratio,fail_ill_ratio)
     @current_target_file = OutputWriter.instance.create_file(prefix: setting_file_name)
   end
 
-  def restore_outputfile(prefix: setting_file_name)
-    @current_target_file = OutputWriter.instance.create_file(prefix: prefix)
-  end
-  
   def set_hash(healthy,ill)
     @watcher_init_ratio_map = {}
     @watcher_init_ratio_map = {:healthy => healthy, :ill => ill}
   end
 
+  #シミュレーション実行のメイン部分
+  #ここで、高齢者の加齢 -> 加齢とともに病気になりやすくなるなどを表現
   def simulate
     @watcher = Watcher.new(@watcher_init_ratio_map)
     judged_state = @watcher.judge_state(@elderly.current_state)
@@ -56,6 +55,8 @@ class Main
     @elderly.move_state()
     unless @elderly.current_state == nil
       OutputWriter.instance.write_to_file(@current_target_file,"#{@elderly.current_state},#{judged_state}\n") 
+    else
+      p "高齢者の現在状態がありません！"
     end
   end 
 
