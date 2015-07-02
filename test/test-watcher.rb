5108c5ab83ad27d403f151240773b33938411701
S.Tokunaga <otokuna46@gmail.com>
2015-07-02 11:16:13 +0900

    fix require file

--- test/test-watcher.rb
+++ test/test-watcher.rb
@@ -1,19 +1,9 @@
 require 'minitest/autorun'
 require 'minitest/unit'
-require 'test-run.rb/watcher.rb'
+require '../watcher.rb'
 
 class TestWatcher < MiniTest::Test
   def setup
     @watcher = Watcher.new({:healthy => 0.6,:ill => 0.3})
   end
-
-  #expect fail
-  def test_random_val
-    assert_nil @watcher.generate_random
-  end
-
-  def test_bar
-    assert_equal 0,0
-  end
-
 end
