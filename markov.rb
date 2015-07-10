9143d54b1dd842bef16cdcc14b333887cb92cee0
S.tokunaga <tokunaga@ws.cs.kobe-u.ac.jp>
2015-07-10 17:03:08 +0900

    モデルの修正 ->失敗する確率を健康と病気で意味を同じにした

--- markov.rb
+++ markov.rb
@@ -4,11 +4,11 @@ class Markov
     @current_state=initial_state
   end
   def guard_parameter
-    if @q01.to_f > 0.9999
+    if @q01.to_f > 0.99999
       @q01 = 1
     end
-    if @q10.to_f > 0.9999
-      @q10 = 0.9999
+    if @q10.to_f > 0.99999
+      @q01 = 1
     elsif @q10.to_f < 0
       @q10 = 0
     end
@@ -23,9 +23,9 @@ class Markov
         end
       when 1 then
         if random.to_f < @q10.to_f
-          @current_state=0
-        elsif @q10.to_f < random.to_f then
           @current_state=1
+        elsif @q10.to_f < random.to_f then
+          @current_state=0
         end
     end
     @current_state
