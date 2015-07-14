42aea4a169ab75a1d286a51a6d900962581398a5
S.tokunaga <otokun46@gmail.com>
2015-07-14 10:09:01 +0900

    平均を計算スクリプトの追加

--- output/calc/result/calc_average.sh
+++ output/calc/result/calc_average.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-declare -a country_list=("india" "china" "russia" "africa" "ghana" "mexico")
+#declare -a country_list=("india" "china" "russia" "africa" "ghana" "mexico")
+declare -a country_list=("china")
 
 #５年ごとに集計されたファイルの平均値を算出して、外部ファイルに吐き出す
 function calc_average(){
   _country=$1
   _file_index=$2
   _years=$3
-  cat ./result$_country$_file_index*_$_years"Y".csv | awk 'BEGIN{FS = ","}{k+=$1}{l+=$2}{m+=$3}{n+=$4}END{print k/NR,l/NR,m/NR,n/NR}' >> ./counting/$_years\_result$_country$_file_index.csv
+  cat ./result$_country$_file_index*_$_years"Y".csv | awk 'BEGIN{FS = ","}{k+=$1}{l+=$2}{m+=$3}{n+=$4}END{print k/NR,l/NR,m/NR,n/NR}' >> ./counting/$_country/$_years/$_years\_result$_country$_file_index.csv
 }
 for country in ${country_list[@]}; 
 do
